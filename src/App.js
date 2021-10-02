768b166a81b431860b68fd3479bf6270cb9ae523
Tetiana Blizniukova <tatbliz30@gmail.com>
2021-10-03 01:53:09 +0300

    task-2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Profile from './components/Profile/Profile';
+import Statistics from './components/Statistics/Statistics';
 import user from './data/user.json';
+import statisticalData from './data/statistical-data.json';
 import './App.css';
 
 function App() {
@@ -12,8 +14,10 @@ function App() {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" stats={statisticalData} />
+      <Statistics stats={statisticalData} />
     </div>
-  );
+  )
 }
 
 export default App;
