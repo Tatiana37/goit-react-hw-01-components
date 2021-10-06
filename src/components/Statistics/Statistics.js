76957785b4392fdf8591ca3f804bffba7a23fb0b
Tetiana Blizniukova <tatbliz30@gmail.com>
2021-10-06 21:04:53 +0300

    update

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,6 +1,11 @@
 import PropTypes from 'prop-types';
 import s from './Statistics.module.css';
 
+const createRandomColor = () => {
+  return `rgb(${(Math.random() * 255) << 0}, ${(Math.random() * 255) << 0}, ${
+    (Math.random() * 255) << 0
+  })`;
+};
 export default function Statistics({ title, stats }) {
   return (
     <section className={s.statistics}>
@@ -8,23 +13,27 @@ export default function Statistics({ title, stats }) {
 
       <ul className={s.statList}>
         {stats.map(({ id, label, percentage }) => (
-          <li className={s.item} key={id}>
+          <li
+            className={s.item}
+            key={id}
+            style={{ backgroundColor: createRandomColor() }}
+          >
             <span className={s.label}>{label}</span>
             <span className={s.percentage}>{percentage}%</span>
           </li>
         ))}
       </ul>
     </section>
-  )
+  );
 }
 
 Statistics.propTypes = {
   title: PropTypes.string,
   stats: PropTypes.arrayOf(
     PropTypes.exact({
-          id: PropTypes.string.isRequired,
-          label: PropTypes.string.isRequired,
-          percentage: PropTypes.number.isRequired,
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
     }).isRequired,
-  )
-}
+  ),
+};
